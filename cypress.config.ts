d29730cc39088c886e04de14aee2f839b6bb4987
JSD <js.dohil@gmail.com>
2024-11-13 16:54:51 -0800

    \ test command change3

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'https://challange20.onrender.com/:10000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
