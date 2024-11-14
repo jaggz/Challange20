861d93391a57c031daf2849f27b83362c379f8cb
JSD <js.dohil@gmail.com>
2024-11-13 17:14:23 -0800

    \ url added

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'https://challange20.onrender.com/:10000',
+    baseUrl: 'https://challange20.onrender.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
